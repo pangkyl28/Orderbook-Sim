7121af3b17067a28e066d2fceeaccd19d2871b1c
pangkyl28 <kylerpang2028@u.northwestern.edu>
2026-02-12 23:28:36 -0600

    add_limit, cancel_order

--- include/ob/Orderbook.hpp
+++ include/ob/Orderbook.hpp
@@ -1,13 +1,17 @@
 #pragma once
+
+using namespace std;
+
 #include "Order.hpp"
 #include "Trade.hpp"
 #include <vector>
+#include <algorithm>
 
 namespace ob {
     class OrderBook {
         public:
             // should return the list of trades executed as a result of adding the order
-            std::vector<Trade> add_limit(const Order& order); 
+            vector<Trade> add_limit(const Order& order); 
 
             // should cancel the order with given order_id
             // return 0 on success, -1 if order_id not found
@@ -23,6 +27,10 @@ namespace ob {
             // helper functions and other stuff for order matching and trade execution
             int cur_trade_id = 0;
             int cur_seq = 0;
+
+            // for now use a vector to store orders, will need a more efficient structure for real implementation
+            vector<Order> bids;
+            vector<Order> asks;
     };
 
 }
